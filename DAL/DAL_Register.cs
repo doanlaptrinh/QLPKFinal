e1c70fd27e048e7400c5beb8f93b838d23b25176
Lý Thành Đạt <65280139+thanhdatly@users.noreply.github.com>
2020-07-15 08:05:32 +0700

    Add files via upload

---
+++ DAL/DAL_Register.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using DTO;
+namespace DAL
+{
+        public class DAL_Register:DBConnect
+        {
+        public bool themTaiKhoan(DTO_Register tk)
+        {
+
+            try
+            {
+                _conn.Open();
+                string cm = "insert into NguoiDung values(@taikhoan,@matkhau,@phanquyen,@ten)";
+                SqlCommand cmd;
+                cmd = new SqlCommand(cm, _conn);
+                cmd.Parameters.AddWithValue("@taikhoan", tk.nguoiDung_taiKhoan);
+                cmd.Parameters.AddWithValue("@matkhau", tk.nguoiDung_matKhau);
+                cmd.Parameters.AddWithValue("@phanquyen",tk.nguoiDung_phanQuyen);
+                cmd.Parameters.AddWithValue("@ten", tk.nguoiDung_Ten);
+                if (cmd.ExecuteNonQuery() > 0)
+                    return true;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                _conn.Close();
+            }
+            return false;
+
+        }
+        public bool CheckTrungTaiKhoan(string taikhoan)
+        {
+            try
+            {
+                _conn.Open();
+                string sql = "select * from NguoiDung where TaiKhoan='" + taikhoan + "' ";
+                SqlCommand cmdd = new SqlCommand(sql, _conn);
+                SqlDataReader dta = cmdd.ExecuteReader();
+                if (dta.Read() == true)
+                {
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                _conn.Close();
+            }
+            return false;
+        }
+        public bool SuaTaiKhoan(DTO_Register tk)
+        {
+            try
+            {
+                // Ket noi
+                _conn.Open();
+
+                // Query string
+                string cm = "UPDATE NguoiDung SET MatKhau=@matkhau,PhanQuyen=@phanquyen,TenNhanVien=@ten where TaiKhoan=@taikhoan";
+                SqlCommand cmd = new SqlCommand(cm, _conn);
+                cmd.Parameters.AddWithValue("@taikhoan", tk.nguoiDung_taiKhoan);
+                cmd.Parameters.AddWithValue("@matkhau", tk.nguoiDung_matKhau);
+                cmd.Parameters.AddWithValue("@phanquyen", tk.nguoiDung_phanQuyen);
+                cmd.Parameters.AddWithValue("@ten", tk.nguoiDung_Ten);
+                if (cmd.ExecuteNonQuery() > 0)
+                    return true;
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+
+                _conn.Close();
+            }
+
+            return false;
+        }
+        public bool XoaTaiKhoan(DTO_Register tk)
+        {
+            try
+            {
+                // Ket noi
+                _conn.Open();
+                string cm = "Delete NguoiDung where TaiKhoan=@taikhoan";
+                SqlCommand cmd = new SqlCommand(cm, _conn);
+                cmd.Parameters.AddWithValue("@taikhoan", tk.nguoiDung_taiKhoan);
+                if (cmd.ExecuteNonQuery() > 0)
+                    return true;
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+
+                _conn.Close();
+            }
+
+            return false;
+        }
+    }
+}
---
+++ DAL/DAL_Thuoc.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAL
+{
+    public class DAL_Thuoc:DBConnect
+    {
+        public bool ThemThuoc(DTO_Thuoc th)
+        {
+
+            try
+            {
+                _conn.Open();
+                // Create List Sql Parameter
+                string cm = "insert into DonThuoc values(@ID,@TenThuoc,@DonViTinh,@LieuDung,@DonGia,@GhiChu)";
+
+                SqlCommand cmd;
+                cmd = new SqlCommand(cm, _conn);
+                cmd.Parameters.AddWithValue("@ID", th.thuoc_id);
+                cmd.Parameters.AddWithValue("@TenThuoc", th.thuoc_tenthuoc);
+                cmd.Parameters.AddWithValue("@DonViTinh", th.thuoc_soLuong);
+                cmd.Parameters.AddWithValue("@LieuDung", th.thuoc_lieuDung);
+                cmd.Parameters.AddWithValue("@DonGia", th.thuoc_donGia);
+                cmd.Parameters.AddWithValue("@GhiChu", th.thuoc_ghiChu);
+                
+
+                if (cmd.ExecuteNonQuery() > 0)
+                    return true;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                _conn.Close();
+            }
+            return false;
+
+        }
+        public bool SuaThuoc(DTO_Thuoc th)
+        {
+
+            try
+            {
+                _conn.Open();
+                // Create List Sql Parameter
+                string cm = "update  DonThuoc set TenThuoc=@tenthuoc,DonViTinh=@donvitinh,LieuDung=@lieudung,DonGia=@dongia,GhiChu=@ghichu where ID=@id";
+
+                SqlCommand cmd;
+                cmd = new SqlCommand(cm, _conn);
+                cmd.Parameters.AddWithValue("@ID", th.thuoc_id);
+                cmd.Parameters.AddWithValue("@tenthuoc", th.thuoc_tenthuoc);
+                cmd.Parameters.AddWithValue("@donvitinh", th.thuoc_soLuong);
+                cmd.Parameters.AddWithValue("@lieudung", th.thuoc_lieuDung);
+                cmd.Parameters.AddWithValue("@dongia", th.thuoc_donGia);
+                cmd.Parameters.AddWithValue("@ghichu", th.thuoc_ghiChu);
+
+
+                if (cmd.ExecuteNonQuery() > 0)
+                    return true;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                _conn.Close();
+            }
+            return false;
+
+        }
+        public bool XoaThuoc(DTO_Thuoc th)
+        {
+            try
+            {
+                _conn.Open();
+                string cm = "Delete DonThuoc where ID =@iD";
+                SqlCommand cmd = new SqlCommand(cm, _conn);
+                cmd.Parameters.AddWithValue("@iD", th.thuoc_id);
+                if (cmd.ExecuteNonQuery() > 0)
+                    return true;
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+
+                _conn.Close();
+            }
+
+            return false;
+        }
+        public bool CheckTrungID(string id)
+        {
+            try
+            {
+                _conn.Open();
+                string sql = "select *from DonThuoc where ID='" + id + "' ";
+                SqlCommand cmdd = new SqlCommand(sql, _conn);
+                SqlDataReader dta = cmdd.ExecuteReader();
+                if (dta.Read() == true)
+                {
+                    return true;
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                _conn.Close();
+            }
+            return false;
+        }
+    }
+}
