472ba4c031629dd3d6cebfd51890b1dd0bffef88
KhanhHuynh31 <65646959+KhanhHuynh31@users.noreply.github.com>
2020-07-15 12:56:15 +0700

    Add files via upload

---
+++ BUS/BUS_DoanhThu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using DAL;
+using System.Data;
+
+namespace BUS
+{
+    public class BUS_DoanhThu
+    {
+        DAL_DoanhThu daldoanhthu = new DAL_DoanhThu();
+        public DataTable Locdoanhthu(DTO_DoanhThu a)
+        {
+            return daldoanhthu.LocDoanhThu(a.Ngaybatdau,a.Ngayketthuc);
+        }
+    }
+    
+}
---
+++ BUS/BUS_TrangChu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+namespace BUS
+{
+    public class BUS_TrangChu
+    {
+        Code_hotro ht=new Code_hotro();
+        public bool phanQuyen(DTO_TrangChu q)
+        {
+            return ht.checkQuyen(q);
+        }
+    }
+}
