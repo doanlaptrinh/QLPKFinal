d1a6927cbd62601a8351968ef9ad3e26375a052a
Lý Thành Đạt <65280139+thanhdatly@users.noreply.github.com>
2020-07-15 08:04:21 +0700

    Add files via upload

---
+++ BUS/BUS_Registercs.cs
@@ -0,0 +1,37 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class BUS_Registercs
+    {
+        DAL_Register dalRE = new DAL_Register();
+        Code_hotro codehotro = new Code_hotro();
+
+        public bool ThemTaiKhoan(DTO_Register tk)
+        {
+            return dalRE.themTaiKhoan(tk);
+        }
+        public bool CheckTrungTaiKhoan(string tk)
+        {
+            return dalRE.CheckTrungTaiKhoan(tk);
+        }
+        public bool checktk(string check)
+        {
+            return codehotro.checktk(check);
+        }
+        public bool SuaTaiKhoan(DTO_Register tk)
+        {
+            return dalRE.SuaTaiKhoan(tk);
+        }
+        public bool XoaTaiKhoan(DTO_Register tk)
+        {
+            return dalRE.XoaTaiKhoan(tk);
+        }
+    }
+}
---
+++ BUS/BUS_Thuoc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+namespace BUS
+{
+    public class BUS_Thuoc
+    {
+        DAL_Thuoc dal_Thuoc = new DAL_Thuoc();
+        public bool ThemThuoc(DTO_Thuoc thuoc)
+        {
+
+            return dal_Thuoc.ThemThuoc(thuoc);
+        }
+        public bool CheckTrung(string id)
+        {
+            return dal_Thuoc.CheckTrungID(id);
+        }
+        public bool SuaThuoc(DTO_Thuoc thuoc)
+        {
+
+            return dal_Thuoc.SuaThuoc(thuoc);
+        }
+        public bool XoaThuoc(DTO_Thuoc thuoc)
+        {
+
+            return dal_Thuoc.XoaThuoc(thuoc);
+        }
+    }
+}
